98124023b940b0fd8dc86160a1f1eefd93c2f003
erickwirley <erickwirley@gmail.com>
2026-02-21 20:40:46 -0300

    tipos primitivos

---
+++ tipos-variaveis/src/aula/tiposvariaveis/TiposVariaveis.java
@@ -0,0 +1,17 @@
+package aula.tiposvariaveis;
+
+public class TiposVariaveis {
+    public static void main(String[] args) throws Exception {
+        // tipos primitivos 
+
+        byte idade = 25;
+        int populacao = 1500000;
+        long populacaoMundial = 8000000000L;
+        float preco = 10.99f;
+        double pi = 3.14159;
+        char inicial = 'J';
+        boolean ativo = true;
+
+        System.out.println(idade + " Idade");
+    }
+}
